ffce81d2d21b723b95fba6a90e61ff0d0c079c41
ItsANameToo <itsanametoo@outlook.com>
2018-07-20 19:57:05 +0200

    chore: setup initial structure

---
+++ Crypto/Crypto/Crypto.h
@@ -0,0 +1,20 @@
+//
+// This file is part of Ark Swift Crypto.
+//
+// (c) Ark Ecosystem <info@ark.io>
+//
+// For the full copyright and license information, please view the LICENSE
+// file that was distributed with this source code.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Crypto.
+FOUNDATION_EXPORT double CryptoVersionNumber;
+
+//! Project version string for Crypto.
+FOUNDATION_EXPORT const unsigned char CryptoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Crypto/PublicHeader.h>
+
+
