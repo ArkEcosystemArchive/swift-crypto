7fdfced44183baf26b33fae1981807dde2efc7d1
ItsANameToo <itsanametoo@protonmail.com>
2018-12-07 20:05:44 +0100

    test: transfer serialization

--- Crypto/CryptoTests/Transactions/Serializers/TransferSerializerTests.swift
+++ Crypto/CryptoTests/Transactions/Serializers/TransferSerializerTests.swift
@@ -10,6 +10,53 @@
 import XCTest
 @testable import Crypto
 
-class TransferSerializerTests {
+class TransferSerializerTests: XCTestCase {
     
+    func testSerializeTransfer() {
+        let json = readJson(file: "transfer_passphrase", type: type(of: self))
+        let serialized = json["serialized"] as! String
+        let transaction = ArkDeserializer.deserialize(serialized: serialized)
+        
+        XCTAssertEqual(serialized, ArkSerializer.serialize(transaction: transaction))
+    }
+    
+    func testSerializeTransferSecondSig() {
+        let json = readJson(file: "transfer_second-passphrase", type: type(of: self))
+        let serialized = json["serialized"] as! String
+        let transaction = ArkDeserializer.deserialize(serialized: serialized)
+        
+        XCTAssertEqual(serialized, ArkSerializer.serialize(transaction: transaction))
+    }
+    
+    func testSerializeTransferWithVendorField() {
+        let json = readJson(file: "transfer_passphrase-with-vendor-field", type: type(of: self))
+        let serialized = json["serialized"] as! String
+        let transaction = ArkDeserializer.deserialize(serialized: serialized)
+        
+        XCTAssertEqual(serialized, ArkSerializer.serialize(transaction: transaction))
+    }
+    
+    func testSerializeTransferWithVendorFieldSecondSig() {
+        let json = readJson(file: "transfer_second-passphrase-with-vendor-field", type: type(of: self))
+        let serialized = json["serialized"] as! String
+        let transaction = ArkDeserializer.deserialize(serialized: serialized)
+        
+        XCTAssertEqual(serialized, ArkSerializer.serialize(transaction: transaction))
+    }
+    
+    func testSerializeTransferWithVendorFieldHex() {
+        let json = readJson(file: "transfer_passphrase-with-vendor-field-hex", type: type(of: self))
+        let serialized = json["serialized"] as! String
+        let transaction = ArkDeserializer.deserialize(serialized: serialized)
+        
+        XCTAssertEqual(serialized, ArkSerializer.serialize(transaction: transaction))
+    }
+    
+    func testSerializeTransferWithVendorFieldHexSecondSig() {
+        let json = readJson(file: "transfer_second-passphrase-with-vendor-field-hex", type: type(of: self))
+        let serialized = json["serialized"] as! String
+        let transaction = ArkDeserializer.deserialize(serialized: serialized)
+
+        XCTAssertEqual(serialized, ArkSerializer.serialize(transaction: transaction))
+    }
 }
